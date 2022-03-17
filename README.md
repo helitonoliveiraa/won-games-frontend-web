35fd0b243692a6546363f5e9b3a8f8eb515962f7
Héliton Oliveira <heliton.oliveira88@gmail.com>
2022-03-16 22:36:45 -0300

    Initial commit from Create Next App

---
+++ README.md
@@ -0,0 +1,24 @@
+# Boilerplate - NextJS with Storybook
+
+## :memo: About
+
+This boilerplate was developed using NextJS and is configured with the below technologies.
+
+- [PWA]()
+- [Styled Components]()
+- [Storybook]()
+- [Jest]()
+- [React Testing Library]()
+- [husky]()
+- [commitlint]()
+- [commitizen]()
+
+## :rocket: Star from this project
+
+```bash
+  $ yarn create next-app -f https://github.com/helitonoliveiraa/boilerplate-nextjs-storybook
+
+  $ cd boilerplate-nextjs-storybook
+
+  $ yarn
+```
---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%; // 1REM equal to 10px
+  }
+
+  // This is optional, just setup for this project
+  html, body, #__next {
+    min-height: -webkit-fill-available;
+    height: -webkit-fill-available;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
