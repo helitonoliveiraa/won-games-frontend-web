81ba0bd2fa9bc80b06921afed054d0c17f2cfc1e
Héliton Oliveira <heliton.oliveira88@gmail.com>
2022-03-17 22:32:02 -0300

    chore: setup storybook to work with absolute import file

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,15 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-essentials"
   ],
-  "framework": "@storybook/react"
+  framework: "@storybook/react",
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+
+    return config;
+  }
 }
