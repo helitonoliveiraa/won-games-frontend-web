a812d80f0d228720ac8a2ef7304f9218b562cb25
Héliton Oliveira <heliton.oliveira88@gmail.com>
2022-03-24 00:07:49 -0300

    feat: create help help component to handle with responsivenes between mobiel and dasktop

---
+++ src/components/MediaMatch/MediaMatch.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { MediaMatch } from './index';
+
+export default {
+  title: 'MediaMatch',
+  component: MediaMatch,
+  parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+    },
+  },
+} as ComponentMeta<typeof MediaMatch>;
+
+export const Desktop: ComponentStory<typeof MediaMatch> = () => (
+  <MediaMatch greaterThan="medium">Only on Desktop</MediaMatch>
+);
+
+export const Mobile: ComponentStory<typeof MediaMatch> = () => (
+  <MediaMatch lessThan="medium">Only on Mobile</MediaMatch>
+);
+
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'iphonex',
+  },
+};
