bc916e636c7b5f2e1a1b7c4fb6b77d6ece14b467
Héliton Oliveira <heliton.oliveira88@gmail.com>
2022-03-26 21:53:25 -0300

    chore: setup to resolve absolute import with alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,23 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
 module.exports = {
   stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   addons: [
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-postcss"
   ],
   framework: "@storybook/react",
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
 
+    //Specific config to resolver absolute imports with custom alias
+    config.resolve.plugins.push(new TsconfigPathsPlugin())
+
     return config;
-  }
+  },
 }
+
+
